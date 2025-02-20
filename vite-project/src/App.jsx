4c467252d027e509f5e337b0b06afa7e8e4ecbaf
Prakash <rsprakash051@gmail.com>
2025-02-20 14:26:23 +0530

    Build Done1

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import About from "./components/FunctionalComponents/About"
-import Home from "./components/FunctionalComponents/Home"
-import Form from "./components/FunctionalComponents/Form"
-import Gallery from "./components/FunctionalComponents/Gallery"
-import Skills from "./components/FunctionalComponents/Skills"
+import About from "./Components/FunctionalComponents/About"
+import Home from "./Components/FunctionalComponents/Home"
+import Form from "./Components/FunctionalComponents/Form"
+import Gallery from "./Components/FunctionalComponents/Gallery"
+import Skills from "./Components/FunctionalComponents/Skills"
 import {BrowserRouter ,Routes, Route,} from "react-router-dom"
-import UseEffect from "./components/FunctionalComponents/UseEffect"
-import UseState from "./components/FunctionalComponents/UseState"
-import Login from "./components/FunctionalComponents/Login"
-import SignUp from "./components/FunctionalComponents/SignUp";
-import UseEffectAPI from "./components/FunctionalComponents/UseEffectAPI";
-import UseRef from "./components/FunctionalComponents/UseRef";
-import Navbar from "./components/FunctionalComponents/Navbar";  
-import UseMemo from "./components/FunctionalComponents/UseMemo";
+import UseEffect from "./Components/FunctionalComponents/UseEffect"
+import UseState from "./Components/FunctionalComponents/UseState"
+import Login from "./Components/FunctionalComponents/Login"
+import SignUp from "./Components/FunctionalComponents/SignUp";
+import UseEffectAPI from "./Components/FunctionalComponents/UseEffectAPI";
+import UseRef from "./Components/FunctionalComponents/UseRef";
+import Navbar from "./Components/FunctionalComponents/Navbar";  
+import UseMemo from "./Components/FunctionalComponents/UseMemo";
 function App() {
   return (
       <main>

62647038b7047a34025b763c3440208f1a327a8a
Prakash <rsprakash051@gmail.com>
2025-02-20 14:18:35 +0530

    Build Done1

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import About from "./components/FunctionalComponents/About"
-import Home from "./components/FunctionalComponents/home"
+import Home from "./components/FunctionalComponents/Home"
 import Form from "./components/FunctionalComponents/Form"
 import Gallery from "./components/FunctionalComponents/Gallery"
 import Skills from "./components/FunctionalComponents/Skills"
-import Navbar from "./components/FunctionalComponents/Navbar"
 import {BrowserRouter ,Routes, Route,} from "react-router-dom"
 import UseEffect from "./components/FunctionalComponents/useEffect"
 import UseState from "./components/FunctionalComponents/useState"
@@ -12,6 +11,7 @@ import Login from "./components/FunctionalComponents/Login"
 import SignUp from "./components/FunctionalComponents/SignUp";
 import UseEffectAPI from "./components/FunctionalComponents/useEffectAPI";
 import UseRef from "./components/FunctionalComponents/UseRef";
+import Navbar from "./components/FunctionalComponents/Navbar";  
 import UseMemo from "./components/FunctionalComponents/useMemo";
 function App() {
   return (

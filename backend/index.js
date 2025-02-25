9410fad878e55d58304de0c2276fc59c6c5b5b1b
Prakash <rsprakash051@gmail.com>
2025-02-25 08:16:53 +0530

    DAY-12 AN

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,10 @@ const app=express();
 const port=process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
-    res.send('Hello World');
+    res.sendFile("C:/PRAKASH/SJITMERN/backend/index.html");
 }
 );
+
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);

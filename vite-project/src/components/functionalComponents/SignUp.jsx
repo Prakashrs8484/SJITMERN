c1ba7ca24f507a8cb7811133e8a9490bfadae3e5
Prakash <rsprakash051@gmail.com>
2025-02-25 14:41:15 +0530

    Signup

--- vite-project/src/components/functionalComponents/SignUp.jsx
+++ vite-project/src/components/functionalComponents/SignUp.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-
-const SignUp = () => {
+import { Link } from 'react-router-dom'
+const Signup = () => {
   return (
-    <div>
-      SignUp
+    <div className="container">
+      <form action="" className="form-box">
+        <label htmlFor="UserName">UserName:</label>
+        <input type="text" id='UserName'/>
+        <label htmlFor="Email">Email:</label>
+        <input type="text" id='Email'/>
+        <label htmlFor="Password">Password:</label>
+        <input type="text" id='Password'/>
+        <div style={{textAlign:"center"}}><button>SignUp</button></div>
+        <div style={{marginTop:"10px"}}><span>Old User?</span><Link to='/login'>Login</Link></div>
+      </form>
     </div>
   )
 }
 
-export default SignUp
+export default Signup
